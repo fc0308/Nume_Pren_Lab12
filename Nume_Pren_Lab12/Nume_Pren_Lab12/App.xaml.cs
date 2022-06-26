369eb5d380bc9af58000ee735645913fd4ddc62a
contributor <62571926+fc0308@users.noreply.github.com>
2022-06-26 12:53:08 +0300

    Add project files.

---
+++ Nume_Pren_Lab12/Nume_Pren_Lab12/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Nume_Pren_Lab12.Data;
+
+namespace Nume_Pren_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Nume_Pren_Lab12/Nume_Pren_Lab12/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nume_Pren_Lab12.Models
+{
+    public class ShopList
+    {
+
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
